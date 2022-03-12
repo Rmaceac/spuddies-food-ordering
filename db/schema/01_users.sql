255686004fc66aef9d46a0362d82acb89d605e48
Ryan <ryan.maceachern@gmail.com>
2022-03-11 21:15:37 -0800

    broke db schema into multiple files

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,6 +1,6 @@
 -- Drop and recreate Users table (Example)
 
-DROP TABLE IF EXISTS users, menu, orders, menu_orders CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -9,31 +9,3 @@ CREATE TABLE users (
   phone VARCHAR(50) NOT NULL,
   address VARCHAR(255)
 );
-
-CREATE TABLE orders (
-  id SERIAL PRIMARY KEY NOT NULL,
-  users_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  order_date DATE NOT NULL,
-  total_price INTEGER NOT NULL,
-  quantity INTEGER NOT NULL
-);
-
-CREATE TABLE menu (
-  id SERIAL PRIMARY KEY NOT NULL,
-  item VARCHAR(255) NOT NULL,
-  type VARCHAR(255),
-  price INTEGER NOT NULL,
-  description TEXT,
-  thumbnail_url TEXT
-);
-
-CREATE TABLE menu_orders (
-  id SERIAL PRIMARY KEY NOT NULL,
-  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
-  menu_id INTEGER REFERENCES menu(id) ON DELETE CASCADE,
-  order_date DATE NOT NULL,
-  total_price INTEGER NOT NULL,
-  quantity INTEGER NOT NULL
-);
-
-
---
+++ db/schema/02_menu.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS menu CASCADE;
+CREATE TABLE menu (
+  id SERIAL PRIMARY KEY NOT NULL,
+  item VARCHAR(255) NOT NULL,
+  type VARCHAR(255),
+  price INTEGER NOT NULL,
+  description TEXT,
+  thumbnail_url TEXT
+);
