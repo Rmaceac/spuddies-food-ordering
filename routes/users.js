cab88573803561ca8cd7262981f31af85645c2c8
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-03-11 20:18:08 -0800

    Nothing modified in users.js, but it says it is

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,13 @@
 const express = require('express');
 const router  = express.Router();
 
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
       .then(data => {
         const users = data.rows;
+        console.log(users)
         res.json({ users });
       })
       .catch(err => {
