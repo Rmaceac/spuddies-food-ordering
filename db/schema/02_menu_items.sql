7f5af51a39d78adf576a5e08061ba1c8a5006c5d
Ryan <ryan.maceachern@gmail.com>
2022-03-12 13:17:15 -0800

    refactored ERD, made changes to db tables accordingly

--- db/schema/02_menu_items.sql
+++ db/schema/02_menu_items.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS menu CASCADE;
-CREATE TABLE menu (
+DROP TABLE IF EXISTS menu_items CASCADE;
+CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
   item VARCHAR(255) NOT NULL,
   type VARCHAR(255),
-  price INTEGER NOT NULL,
+  price MONEY NOT NULL,
   description TEXT,
   thumbnail_url TEXT
 );
