c18b28c4a3d48529c9c8dbc02a2c24c2cac2c4ca
vagrant <tpirozzini@gmail.com>
2022-03-13 00:51:49 +0000

    add remove button functionality

--- public/scripts/current-order.js
+++ public/scripts/current-order.js
@@ -31,6 +31,16 @@ const $orderEntries = $('.order-entries');
 const addRemoveBtnListener = (removeBtn) => {
     removeBtn.on('click', (e => {
     e.preventDefault();
+    const tempArr = dataOrder;
+    console.log(e.target.id);
+    tempArr.forEach((item, index) => {
+      console.log(item.id);
+      if(item.id == e.target.id) {
+        dataOrder.splice(index, 1);
+        console.log(dataOrder);
+      }
+    });
+
     const tr = e.target.parentElement.parentElement;
     tr.remove();
   }));
@@ -38,6 +48,7 @@ const addRemoveBtnListener = (removeBtn) => {
 
 const renderOrder = (order) => {
   let totalCost = 0;
+
   for (const item of order) {
     // Create new jquery object newItem
     const newItem = addOrderItem(item);
@@ -47,7 +58,6 @@ const renderOrder = (order) => {
 
     // Add event listener to remove button
     addRemoveBtnListener(removeBtn);
-
     $orderEntries.append(newItem);
   }
 };
@@ -60,12 +70,46 @@ const addOrderItem = (object) => {
       <td>x ${object.quantity}</td>
       <td>$${object.price.toFixed(2)}</td>
       <td>$${(object.price * object.quantity).toFixed(2)}</td>
-      <td><button class="remove-btn">Remove</button></td>
+      <td><button  id=${object.id} class="remove-btn">Remove</button></td>
     </tr>
   `);
   return $orderItem;
 };
 
-renderOrder(dataOrder);
+
+
+
+class Item {
+  constructor(name, quantity, price) {
+    this.name = name;
+    this.quantity = quantity;
+    this.price = price;
+    this.id = () => {
+      generateRandomID();
+    }
+  }
+};
+
+
+  const $burger = $('.burger');
+
+  $burger.on('click', (e) => {
+    dataOrder.push({
+      id: 3,
+      name: 'NEW ITEM',
+      quantity: 3,
+      price: 6.44
+    });
+    $orderEntries.empty();
+    renderOrder(dataOrder);
+  })
+
+
+//
+  // const author = document.querySelector('#author').value;
+  // const pages = document.querySelector('#pages').value;
+
+  // const item = new Item(name, quantity, price, id);
+
 
 });
