d5ba09144e9c27e8e26c620cf59473dc5fcb3f3a
vagrant <tpirozzini@gmail.com>
2022-03-14 04:01:49 +0000

    plus/minus quanity feature - still needs boundarys set

--- public/scripts/order-buttons.js
+++ public/scripts/order-buttons.js
@@ -1,18 +1,26 @@
 $(() => {
 
-  // ADD/SUBTRACT QUANTITY
-  $('.add').click(function() {
-		if ($(this).prev().val() < 9) {
-    	$(this).prev().val(+$(this).prev().val() + 1);
-		}
-	});
+  // INCREASE QUANTITY
+  const increaseQuantity = () => {
+    $('.add').click(function() {
+      if ($(this).prev().val() < 9) {
+        $(this).prev().val(+$(this).prev().val() + 1);
+      }
+    });
+  };
 
-	$('.minus').click(function() {
-			if ($(this).next().val() > 1) {
-				if ($(this).next().val() > 1) $(this).next().val(+$(this).next().val() - 1);
-			}
-	});
+  // DECREASE QUANTITY
+  const decreaseQuantity = () => {
+    $('.minus').click(function() {
+        if ($(this).next().val() > 1) {
+          if ($(this).next().val() > 1) $(this).next().val(+$(this).next().val() - 1);
+        }
+    });
+  }
 
-	
+
 
 });
+
+// export {increaseQuantity};
+// export {decreaseQuantity};
