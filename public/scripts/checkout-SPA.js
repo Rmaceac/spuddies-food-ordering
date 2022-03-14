68cae528685c42539a81ef0c01a71cb0ab70b2f6
vagrant <tpirozzini@gmail.com>
2022-03-14 21:47:48 +0000

    create received order container and style

--- public/scripts/checkout-SPA.js
+++ public/scripts/checkout-SPA.js
@@ -1,54 +1,38 @@
 $(() => {
 
-  console.log('checkout SPA Page')
-
   const $submit = $('.check-out')
   const $add = $('.add')
   const $minus = $('.minus')
   const $remove = $('.remove-btn')
 
-
-  $submit.on('click', (e) => {
+  $submit.on('click', (e) =>  {
     console.log(e);
     //SET TIMEOUT & ADD SPINNER
-    setTimeout(checkout, 4000);
-    $p = $('<p></p>')
-    $burger = $('<p class="spinner">🥔</p>')
+    setTimeout(checkout, 1000);
+    $potato = $('<p class="spinner">🥔</p>')
     $prepareOrder = $('<p>Preparing your order...</p>')
     $('.check-out').text('');
-    $('.check-out').append($p)
-    $p.append($prepareOrder, $burger)
+    $('.check-out').append($prepareOrder, $potato)
 
     // REPLACE SUBMIT WITH ORDER CONFIRMATION
     function checkout() {
     $.ajax('checkout.html', { method: 'GET' })
     .then(function (checkout) {
-      console.log('Success: ', checkout);
       $submit.replaceWith(checkout);
 
-    // REMOVE CURRENT ORDER BUTTONS & STOP SPINNER
-    $add.hide();
-    $minus.hide();
-    $remove.hide();
-    $spinner.hide();
-    // ADD LOADING BAR
-    let loadingBar = $('<i class="fa-solid fa-bars-progress"></i>');
-    var txt2 = $("<p></p>").text("Text.");
-    console.log($('check-out'))
-    $('.loading-bar').append(loadingBar);
-    $('.loading-bar').append(txt2);
+      // REMOVE CURRENT ORDER BUTTONS & STOP SPINNER
+      $add.hide();
+      $minus.hide();
+      $remove.hide();
+      $potato.hide();
 
-
-    // function appendText() {
-    //   var txt1 = "<p>Text.</p>";               // Create element with HTML
-    //   var txt2 = $("<p></p>").text("Text.");   // Create with jQuery
-    //   var txt3 = document.createElement("p");  // Create with DOM
-    //   txt3.innerHTML = "Text.";
-    //   $("body").append(txt1, txt2, txt3);      // Append the new elements
-    // }
+      // ADD LOADING BAR
+      $loadingBar = $('<i class="fa-solid fa-bars-progress"></i>');
+      console.log($('loading-bar'))
+      $('.loading-bar').append($loadingBar);
+    });
+    }
   });
-  }
-})
 
 
 
