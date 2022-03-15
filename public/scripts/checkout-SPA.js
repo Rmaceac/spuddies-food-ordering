ac13d1b8e883d9e8ca22ef0105774d3627b96253
Ryan <ryan.maceachern@gmail.com>
2022-03-14 17:45:17 -0700

    weird commit that might need to be overwritten

--- public/scripts/checkout-SPA.js
+++ public/scripts/checkout-SPA.js
@@ -1,53 +1,60 @@
-require("dotenv").config();
+// const { orderReadyMsg } = require("./sms");
+
+// require("dotenv").config();
+// require("sms");
 
 $(() => {
 
-  
 
-  const $submit = $('.check-out')
-  const $add = $('.add')
-  const $minus = $('.minus')
-  const $remove = $('.remove-btn')
+
+  const $submit = $('.check-out');
+  
 
   $submit.on('click', (e) =>  {
     console.log(e);
     //SET TIMEOUT & ADD SPINNER
-    setTimeout(checkout, 3000);
+    setTimeout(checkout, 2000);
     $potato = $('<p class="spinner">🥔</p>')
     $prepareOrder = $('<p>Preparing your order...</p>')
     $('.check-out').text('');
-    $('.check-out').append($prepareOrder, $potato)
+    $('.check-out').append($prepareOrder, $potato);
 
     // REPLACE SUBMIT WITH ORDER CONFIRMATION
+    /*eslint-disable*/
     function checkout() {
-    $.ajax('checkout.html', { method: 'GET' })
-    .then(function (checkout) {
-      $submit.replaceWith(checkout);
+      $.ajax('checkout.html', { method: 'GET' })
+        .then(function (checkout) {
+          $submit.replaceWith(checkout);
 
-      // REMOVE CURRENT ORDER BUTTONS & STOP SPINNER
-      $add.hide();
-      $minus.hide();
-      $remove.hide();
-      $potato.hide();
+          // REMOVE CURRENT ORDER BUTTONS & STOP SPINNER
+          $add.hide();
+          $minus.hide();
+          $remove.hide();          
+          const $add = $('.add');
+          const $minus = $('.minus');
+          const $remove = $('.remove-btn');
 
-      // ADD LOADING BAR
-      $loadingBar = $('<i class="fa-solid fa-bars-progress progress-bar"></i>');
-      $('.loading-bar').append($loadingBar);
-      // //SET TIMEOUT ON PROGRESS BAR
-      // $cooking = $('')
-      // setTimeout(cooking, 10000);
-      // function cooking() {
-      //   $loadingBar.replaceWith($packaging)
-      // }
-      // setTimeout(packaging, 15000);
-      // function packaging() {
-      //   $packaging.replaceWith($pickup)
-      // }
-      // setTimeout(pickup, 20000);
-      //   $pickup.replaceWith($complete)
+          // ADD LOADING BAR
+          /*eslint-enable*/
+          $loadingBar = $('<i class="fa-solid fa-bars-progress progress-bar"></i>');
+          $('.loading-bar').append($loadingBar);
+          // //SET TIMEOUT ON PROGRESS BAR
+          // $cooking = $('')
+          // setTimeout(cooking, 10000);
+          // function cooking() {
+          //   $loadingBar.replaceWith($packaging)
+          // }
+          // setTimeout(packaging, 15000);
+          // function packaging() {
+          //   $packaging.replaceWith($pickup)
+          // }
+          // setTimeout(pickup, 20000);
+          //   $pickup.replaceWith($complete)
 
-    });
-    }
+        });
+    };
+
+    orderReadyMsg();
   });
 
 
