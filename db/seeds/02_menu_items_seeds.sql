45b35d19d791125c19cf802e1a475706eef40dee
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-03-16 14:50:06 -0700

    Corrected a few more img file paths

--- db/seeds/02_menu_items_seeds.sql
+++ db/seeds/02_menu_items_seeds.sql
@@ -2,8 +2,8 @@ INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('
 INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Mashed Tater Burger', 'burger', 9.00, 'Our classic potato burger... mashed!', './images/mashed-tater-burger.jpeg' );
 INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Potato Skin Burger', 'burger', 7.00, 'A modern take on our classic.', './images/potato-skin-burger.jpeg');
 INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Fried Potato Sandwich', 'burger', 6.50, 'Grilled cheese with potato.', './images/potato-skin-burger.jpeg');
-INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Potato Wrap', 'burger', 9.00, 'Not exactly a burger, but this thing has potatos for days!', './images/potato-skin-burger.jpeg');
-INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Stuffed Potato Burger', 'burger', 13.00, 'A burger inside a burger, wow.', './images/potato-skin-burger.jpeg');
+INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Potato Wrap', 'burger', 9.00, 'Not exactly a burger, but this thing has potatos for days!', './images/wrap.jpg');
+INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Stuffed Potato Burger', 'burger', 13.00, 'A burger inside a burger, wow.', './images/stuffed-burger.jpg');
 
 INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Thinly-Sliced Potatos', 'side', 4.00, 'Pretty much exactly what it sounds like.', './images/thinly-sliced-potatoes.jpg');
 INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Crunchy Chips', 'side', 3.00, 'Thinly sliced, but fried a bit differently so theyre crunchy', './images/chips.jpg');
@@ -12,7 +12,7 @@ INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('
 INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Mystery Potato Monstrosity', 'side', 9.00, 'We have no idea what this is. We just found it on the internet.', './images/weird-potato.jpg');
 INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Just Potatoes', 'side', 4.00, 'Theyre everywhere, please help and take some with you.', './images/potatoes.jpg');
 
-INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Tot Pop', 'drink', 2.00, 'Cold, refreshing, liquid potato!', './images/tot-pop.jpeg');
+INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Tot Pop', 'drink', 2.00, 'Cold, refreshing, liquid potato!', './images/tot-pop.jpg');
 INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Tater Ale', 'drink', 2.00, 'Fizzy, ginger potato drink.', './images/tater-ale.jpg');
 INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Orange Potato Fizz', 'drink', 2.00, 'Tropical potato drink.', './images/orange-drink.jpg');
 INSERT INTO menu_items (item, type, price, description, thumbnail_url) VALUES ('Sweet Potato Shake', 'drink', 1.00, 'Creamy delicious milk... err, potato shake.', './images/sweet-potato-shake.jpeg');
