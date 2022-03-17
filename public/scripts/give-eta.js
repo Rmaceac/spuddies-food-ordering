5b01ebec0325763383f9e42d40336d2815f90d5c
vagrant <tpirozzini@gmail.com>
2022-03-17 23:48:08 +0000

    testing ajax with jquery

--- public/scripts/give-eta.js
+++ public/scripts/give-eta.js
@@ -1,16 +1,17 @@
 $(() => {
-  
+
+
   const $submit = $('#eta');
   const $estimate = $('#estimate');
 
   $submit.on('click', function(e) {
     e.preventDefault();
-    
+
     // Text from estimate input field
     const estimate = {
       eta: $estimate.val()
     };
-    
+
     $.ajax({
       url: "http://localhost:8084/api/submit/eta",
       method: "POST",
--- routes/submit.js
+++ routes/submit.js
@@ -8,6 +8,9 @@
 const express = require('express');
 const { orderReadyMsg, orderEstimate, orderSubmitted } = require('../public/scripts/sms');
 const router  = express.Router();
+const etaObj = {
+  eta: "test"
+};
 
 
 module.exports = (db) => {
@@ -15,31 +18,36 @@ module.exports = (db) => {
 
   router.post("/eta", (req, res) => {
     const eta = req.body.eta;
-    orderEstimate(eta);
+    // orderEstimate(eta);
+    etaObj["eta"] = eta;
 
     res.send("Notification Sent");
 
     const time = Number(eta.slice(0, 2));
-    setTimeout(
-      orderReadyMsg, time * 1000);
+    // setTimeout(
+      // orderReadyMsg, time * 1000);
+  });
+
+  router.get('/eta', (req, res) => {
+    res.json(etaObj);
   });
 
   router.get("/", (req, res) => {
     console.log("Order received");
     // orderSubmitted();
   });
-  
+
   router.post("/order", (req, res) => {
     console.log("Post request made to /api/submit/order");
     // console.log(`Req.body: ${req.body.getOrder}`);
-    
+
     const queryStringTotal = `INSERT INTO orders (user_id, total_price) VALUES (2, $1) RETURNING *;`;
     const orderItem = `INSERT INTO order_items (order_id, menu_items_id, quantity, sub_total) VALUES ($1, $2, $3, $4);`;
-    
+
     // calculates total of each subtoal of each item
     const total = req.body.getOrder.reduce((a,c) => Number(a) + Number(c.subtotal), 0);
     // console.log("Total:", total);
-    
+
     let promises = [];
     let params = [];
     let orderID;
