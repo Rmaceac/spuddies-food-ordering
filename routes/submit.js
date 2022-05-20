4629c03c0a73e92285698e34c74aa694363a96c1
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-19 17:00:33 -0700

    begin deployment

--- routes/submit.js
+++ routes/submit.js
@@ -6,7 +6,7 @@
  */
 
 const express = require('express');
-const { orderReadyMsg, orderEstimate, orderSubmitted } = require('../public/scripts/sms');
+const { orderReadyMsg, orderEstimate, orderSubmitted } = require('public/scripts/sms');
 const router  = express.Router();
 const etaObj = {
   eta: "test"
